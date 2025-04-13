22318ebd390c417d6947a36d70822afaa1924c33
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-13 14:37:56 +0300

    importing countdowntimer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CountDownTimer from './components/CountDown';
 
 function App() {
   return (
     <div className="App">
-      
+      <CountDownTimer />
     </div>
   );
 }
