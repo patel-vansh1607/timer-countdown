2984e4b7e86221b691f0f8c705266defbf4d5903
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-13 14:53:19 +0300

    Refactor countdown timer functions and improve timer initialization logic

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -3,9 +3,41 @@ import { useEffect, useRef } from "react";
 const CountDownTimer = () => {
     const [timer, setTimer] = useState("00:00:00");
     const Ref = useRef()
+
+
+
+
+
+    function getTimeRemaining(e) {
+        const total = DataTransfer.parse(e) - Date.parse(new Date ())
+    }
+    function startTimer(e){
+        let{total, hour, minute, seconds} - getTimeRemaining(e)
+    }
+
+
+
+
+
+
+    function clearTimer(e){
+        setTimer("00:00:10")
+        if (Ref.current) {
+            clearInterval(Ref.current);
+            const id = setInterval(() => {
+                startTimer(e)
+            }, 1000)
+        }
+        Ref.current = id;
+    }       
+    function getDeadTime(){
+        let deadline = new Date()
+        deadline.setSeconds(deadline.getSeconds() + 10);
+        return deadline;
+    }
      useEffect (() => {
         getDeadTime( )
-     })
+     }, [])
     return(
         <div className="main-div">
             <div className="countdown-timer">
